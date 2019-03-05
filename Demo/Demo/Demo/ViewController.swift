d5bdca36db06d6a7d0f70e9b16127c09d1ccb8d9
Wuan Fu <quang.phu@asiantech.vn>
2019-03-05 09:39:34 +0700

    Removed file that shouldn't be tracked

---
+++ Demo/Demo/Demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Demo
+//
+//  Created by Quang Phu C M on 3/5/19.
+//  Copyright © 2019 Quang Phu C M. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
